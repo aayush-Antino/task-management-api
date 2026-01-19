141ad397998ce1509a0c9eb3689d698bdea79c62
aayush-Antino <aayush.chauhan@antino.com>
2026-01-19 15:04:59 +0530

    add jwt

---
+++ src/utils/jwt.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+
+// Generate Access Token (15 min)
+const generateAccessToken = (payload) => {
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+};
+
+// Generate Refresh Token (7 days)
+const generateRefreshToken = (payload) => {
+  return jwt.sign(payload, REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+// Verify Token
+const verifyToken = (token, type = "access") => {
+  const secret =
+    type === "access" ? ACCESS_TOKEN_SECRET : REFRESH_TOKEN_SECRET;
+
+  return jwt.verify(token, secret);
+};
+
+module.exports = {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyToken,
+};
